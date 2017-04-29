543d9d94ada09ab0d742191169ecec92f0f33a98
Becca Harris <bharris@providigm.com>
2017-04-28 21:45:16 -0600

    trying again

--- src/app/pvdm-admission-assessment/pvdm-admission-assessment.component.ts
+++ src/app/pvdm-admission-assessment/pvdm-admission-assessment.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms'
 
-
-export class Admission {
-    master_no = ''
-}
-
 @Component({
   selector: 'pvdm-admission-assessment',
   templateUrl: './pvdm-admission-assessment.component.html',
   styleUrls: ['./pvdm-admission-assessment.component.scss']
 })
 export class PvdmAdmissionAssessmentComponent {
-  name = new FormControl();
+  master_no = new FormControl();
 }
