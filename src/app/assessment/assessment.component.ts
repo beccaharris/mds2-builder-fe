24eed2cd0c59f34e6cf7fe6cff21e5b410762bba
Becca Harris <bharris@providigm.com>
2017-05-02 14:58:18 -0600

    building assessment through c6

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import { AdmissionType, AssessmentReason } from './assessment.constants';
-
+import { AdmissionType, AssessmentReason, DischargeType } from './assessment.constants';
+import { ComatoseState, MemoryState, IndependenceState, HearingAbility } from './assessment.constants'
+import { YesNoAnswer, UnderstoodAbility, SpeechClarity, UnderstandingAbility } from './assessment.constants'
 
 @Component({
   selector: 'assessment',
   templateUrl: './assessment.component.html',
   styleUrls: ['./assessment.component.scss']
 })
+
 export class AssessmentComponent {
+  
   mdsAssessment: FormGroup;
   admissionType = AdmissionType;
   assessmentReason = AssessmentReason;
+  dischargeType = DischargeType;
+  comatoseState = ComatoseState;
+  memoryState = MemoryState;
+  independenceState = IndependenceState;
+  hearingAbility = HearingAbility;
+  yesnoAnswer = YesNoAnswer;
+  understoodAbility = UnderstoodAbility;
+  speechClarity = SpeechClarity;
+  understandingAbility = UnderstandingAbility;
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -22,6 +34,21 @@ export class AssessmentComponent {
       resident_identifier: '1234567890',
       aa7: '01',
       aa8: '01', 
+      aa9: '--',
+      ab1: '20170422',
+      a3: '20170422',
+      b1: '0',
+      b2a: '--',
+      b4: '0',
+      c1: '0',
+      c3b: '0',
+      c3c: '0',
+      c3d: '0',
+      c3e: '0',
+      c3f: '0',
+      c4: '0',
+      c5: '0',
+      c6: '0',
     })
   }
 }
--- src/app/assessment/assessment.constants.ts
+++ src/app/assessment/assessment.constants.ts
@@ -10,3 +10,58 @@ export const AssessmentReason = [
   { key: '05', label: '05 - Quarterly Review Assessment'},
   { key: '10', label: '10 - Significant Change of Prior Quarterly Assessment'}
 ];
+export const DischargeType = [
+  { key: '--', label: '--'},
+  { key: '06', label: '06 - Discharge — Return Not Anticipated'},
+  { key: '07', label: '07 - Discharge — Return Anticipated'},
+  { key: '08', label: '08 - Discharged Prior to Completing Initial Assessment'},
+];
+export const ComatoseState = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - Not comatose or semi-comatose'},
+  { key: '1', label: '1 - Comatose or in a persistent vegetative state'}
+];
+export const MemoryState = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - No memory problem'},
+  { key: '1', label: '1 - Memory problem'}
+];
+export const IndependenceState = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - Independent'},
+  { key: '1', label: '1 - Modified Independence'},
+  { key: '2', label: '2 - Moderately Impaired'},
+  { key: '3', label: '3 - Severely Impaired'},
+];
+export const HearingAbility = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - Hears Adequately'},
+  { key: '1', label: '1 - Minimal Difficulty'},
+  { key: '2', label: '2 - Hears in special situations only'},
+  { key: '3', label: '3 - Severely Impaired'},
+];
+export const YesNoAnswer = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - No'},
+  { key: '1', label: '1 - Yes'},
+];
+export const UnderstoodAbility = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - Understood'},
+  { key: '1', label: '1 - Usually Understood'},
+  { key: '2', label: '2 - Sometimes Understood'},
+  { key: '3', label: '3 - Rarely or Never Understood'},
+];
+export const SpeechClarity = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - No Change'},
+  { key: '1', label: '1 - Improved'},
+  { key: '2', label: '2 - Deteriorated'},
+];
+export const UnderstandingAbility = [
+  { key: '--', label: '--'},
+  { key: '0', label: '0 - Understands'},
+  { key: '1', label: '1 - Usually Understands'},
+  { key: '2', label: '2 - Sometimes Understands'},
+  { key: '3', label: '3 - Rarely or Never Understands'},
+]
