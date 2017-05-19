d43be3c9c6f82a115fe05b0fa00af7104c90bd87
Becca Harris <bharris@providigm.com>
2017-05-18 18:21:50 -0600

    styling the new button

--- src/app/main-header/main-header.component.spec.ts
+++ src/app/main-header/main-header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PvdmHeaderComponent } from './pvdm-header.component';
+import { HeaderComponent } from './main-header.component';
 
 describe('PvdmHeaderComponent', () => {
-  let component: PvdmHeaderComponent;
-  let fixture: ComponentFixture<PvdmHeaderComponent>;
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PvdmHeaderComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PvdmHeaderComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
